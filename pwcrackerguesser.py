723bba21e653f496930356893583c05123c25a3d
WolfverusWasTaken <75195899+WolfverusWasTaken@users.noreply.github.com>
2020-11-29 15:01:03 +0800

    Update pwcrackerguesser.py

--- pwcrackerguesser.py
+++ pwcrackerguesser.py
@@ -1,9 +1,6 @@
-# Imports
-import itertools
 import time
+import itertools
 
-
-# Brute force function
 def passwordtest(SetPass, SetString):
     start = time.time()
     chars = SetString
@@ -15,12 +12,11 @@ def passwordtest(SetPass, SetString):
             letter = ''.join(letter)
             if letter == SetPass:
                 end = time.time()
-                distance = end - start
-                return (attempts, distance)
+                timetaken = end - start
+                return (timetaken)
 
 
 pw = input("Key in password here :")
-# Allowed characters
 stringType = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_-+=[{]}|:;'\",<.>/?"
-tries, timeAmount = passwordtest(pw, stringType)
+timeAmount = passwordtest(pw, stringType)
 print("Password Cracked in %s seconds!" % (timeAmount))
